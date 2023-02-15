3da73fd82b33094ffb66e17b17feb8faf2df4a9a
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2023-02-15 00:10:42 -0500

    Updated stencil jig

--- scripts/stencil.sh
+++ scripts/stencil.sh
@@ -2,4 +2,4 @@ if [ $(basename $PWD) == "scripts" ]; then
     cd ..
 fi
 
-kikit stencil create  --jigsize 60 60 pcb/2x2MakerspaceKeypad.kicad_pcb pcb/stencil 
+kikit stencil create  --jigsize 60 60 --tolerance 0.2 pcb/2x2MakerspaceKeypad.kicad_pcb pcb/stencil 
